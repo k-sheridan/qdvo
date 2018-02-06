b4116e204e94edba76853bf4d3a1a30164d9947c
Trexter <sheridak@purdue.edu>
2018-02-06 13:15:04 -0500

    derived nice jacobians for BA

---
+++ octave/BA_jacobian.m
@@ -0,0 +1,95 @@
+syms vx vy vz wx wy wz u_curr rx ry rz
+
+% the hat of the se3 lie algebra
+A = [0, -wz, wy, vx;
+    wz, 0, -wx, vy;
+    -wy, wx, 0, vz;
+    0, 0, 0, 0]
+
+%compute first order approximation of transform
+T = eye(4, 4) + A
+
+T_inv = [T(1:3, 1:3)', -T(1:3, 1:3)*T(1:3, 4); T(4, 1:4)]
+
+v = [vx;vy;vz]
+w = [wx;wy;wz]
+
+%compute jacobian for a small camera movement
+
+r = [rx;ry;rz;1] % point position in the current frame (homogenous)
+
+r_p = T_inv*r;
+
+% project onto image plane
+u = r_p(1) / r_p(3);
+v = r_p(2) / r_p(3);
+
+fprintf('du/dvx: %s \n', subs(diff(u, vx), [vx,vy,vz,wx,wy,wz], [0, 0, 0, 0, 0, 0]))
+
+fprintf('du/dvy: %s\n', subs(diff(u, vy), [vx,vy,vz,wx,wy,wz], [0, 0, 0, 0, 0, 0]))
+
+fprintf('du/dvz: %s\n', subs(diff(u, vz), [vx,vy,vz,wx,wy,wz], [0, 0, 0, 0, 0, 0]))
+
+fprintf('du/dwx: %s\n', subs(diff(u, wx), [vx,vy,vz,wx,wy,wz], [0, 0, 0, 0, 0, 0]))
+
+fprintf('du/dwy: %s\n', subs(diff(u, wy), [vx,vy,vz,wx,wy,wz], [0, 0, 0, 0, 0, 0]))
+
+fprintf('du/dwz: %s\n', subs(diff(u, wz), [vx,vy,vz,wx,wy,wz], [0, 0, 0, 0, 0, 0]))
+
+% v part of jacobian
+
+fprintf('\ndv/dvx: %s \n', subs(diff(v, vx), [vx,vy,vz,wx,wy,wz], [0, 0, 0, 0, 0, 0]))
+
+fprintf('dv/dvy: %s\n', subs(diff(v, vy), [vx,vy,vz,wx,wy,wz], [0, 0, 0, 0, 0, 0]))
+
+fprintf('dv/dvz: %s\n', subs(diff(v, vz), [vx,vy,vz,wx,wy,wz], [0, 0, 0, 0, 0, 0]))
+
+fprintf('dv/dwx: %s\n', subs(diff(v, wx), [vx,vy,vz,wx,wy,wz], [0, 0, 0, 0, 0, 0]))
+
+fprintf('dv/dwy: %s\n', subs(diff(v, wy), [vx,vy,vz,wx,wy,wz], [0, 0, 0, 0, 0, 0]))
+
+fprintf('dv/dwz: %s\n', subs(diff(v, wz), [vx,vy,vz,wx,wy,wz], [0, 0, 0, 0, 0, 0]))
+
+% how does changing the position of the point in the current frame change the
+% u and v
+
+fprintf('\ndu/drz: %s\n', subs(diff(u, rz), [vx,vy,vz,wx,wy,wz], [0, 0, 0, 0, 0, 0]))
+
+fprintf('dv/drz: %s\n', subs(diff(v, rz), [vx,vy,vz,wx,wy,wz], [0, 0, 0, 0, 0, 0]))
+
+
+%CLOSED FORM -=-=-=-=-=-=-=-=-= not correct lol
+
+% v = [vx;vy;vz]
+% w = [wx;wy;wz]
+%
+% w_hat = [0, -wz, wy;
+%          wz, 0, -wx;
+%         -wy, wx, 0]
+%
+% % closed form solution to the exponential map of w
+% R = eye(3, 3) + (w_hat/norm(w)) * sin(norm(w)) + (w_hat/norm(w))^2 * (1 - cos(norm(w)));
+%
+% % closed form translation part of the exponential map
+% t = ((eye(3, 3) - R) * w_hat * v + w*w'*v) / norm(w);
+%
+% T = [R, t; zeros(1, 3), 1];
+%
+% % test nonzero w
+% subs(T, [vx,vy,vz,wx,wy,wz], [0, 0, 0, 0.00001, 0, 0])
+%
+%
+% %compute jacobian for a small camera movement
+%
+% r = [rx;ry;rz;1] % point position in the current frame (homogenous)
+%
+% r_p = T*r;
+%
+% % project onto image plane
+% u = r_p(1) / r_p(3);
+% v = r_p(2) / r_p(3);
+%
+% %jacobians are atthe unit transform so a numerical limit
+%
+% %du/dvx
+% subs(diff(u, vx), [vx,vy,vz,wx,wy,wz], [0, 0, 0, 0.000001, 0, 0])
