f2dda95e64ced7763ac97670b8edd38de86285e5
Trexter <sheridak@purdue.edu>
2018-03-13 11:50:08 -0400

    checking algorithm

---
+++ octave/process.m
@@ -0,0 +1,61 @@
+function [state, Sigma, pose_transform] = process(state, Sigma, pose_transform, dt)
+    new_state = convolveState(state, dt);
+    T = se3Exp(new_state(1:6, 1)); % compute the transform that the tangent vector has undergone
+    
+    pose_transform = pose_transform * T
+    
+    % numerically computing the Map into the next state space
+    delta = 1e-3;
+    J = zeros(length(state));
+    for index = (1:length(state))
+        upper = state;
+        lower = state;
+        
+        upper(index) = upper(index) + delta;
+        lower(index) = lower(index) - delta;
+        
+        upper_out = convolveState(upper, dt);
+        lower_out = convolveState(lower, dt);
+        
+        J(:, index) = (upper_out - lower_out) / (2*delta);
+    end
+    
+    % propagate the uncertainty, then transform the the tangent space of
+    % the new pose
+    A = eye(15);
+    A(1:6, 1:6) = se3Adjoint(inv(T));
+    
+    Q = processNoise(dt);
+    
+    Sigma = A * (J*Sigma*J' + Q) * A';
+    
+    new_state(1:6, 1) = zeros(6, 1); % zero the tangent vector again
+    state = new_state;
+end
+
+function [x] = convolveState(x0, dt)
+    
+    dr =  x0(7:9, 1)*dt + 0.5*dt^2*x0(13:15, 1);
+    
+    w = x0(10:12, 1)*dt;
+    
+    T = [so3Exp(w), dr;
+        zeros(1, 3), 1];
+    
+    %compute the new tangent from the rotation and translation
+    x(1:6, 1) = se3Log(se3Exp(x0(1:6, 1)) * T);
+    
+    R = so3Exp(x0(10:12, 1)*dt);
+    
+    x(7:9, 1) = R'*(x0(7:9, 1) + x0(13:15, 1)*dt);
+    
+    x(10:12, 1) = x0(10:12, 1);
+    
+    x(13:15, 1) = R'*(x0(13:15, 1));
+end
+
+
+function [Q] = processNoise(dt)
+    Q = eye(15) * dt*0.1;
+end
+
--- octave/tangentSpaceEKF.m
+++ octave/tangentSpaceEKF.m
@@ -1,6 +1,18 @@
 
 % first 6 dimensions are the tangent space around the current pose (stored externally)
 T = eye(4);
-%state = [tx, ty, tz, thetax, thetay, thetaz, bdx, bdy, bdz, bwx, bwy, bwz]
-state = [0; 0; 0; 0; 0; 0; 1; 0; 0; 0; 0; pi/4];
+%state = [tx, ty, tz, thetax, thetay, thetaz, bdx, bdy, bdz, bwx, bwy, bwz, bax, bay, baz]
+state = [0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; pi/4; 0; 0; 0];
+
+Sigma = zeros(15);
+Sigma(1:6, 1:6) = eye(6)*0.001;
+Sigma(7:15, 7:15) = eye(9)*0.001;
+
+
+dt = 0.01;
+
+for t = (0:dt:10)
+    [state, Sigma, T] = process(state, Sigma, T, dt);
+    drawState(T, Sigma);
+end
 
