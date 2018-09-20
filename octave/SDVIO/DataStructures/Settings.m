b919755e28ef696e1681ecedab3f395c41350c71
Trexter <sheridak@purdue.edu>
2018-09-20 15:41:17 -0400

    reorganize

---
+++ octave/SDVIO/DataStructures/Settings.m
@@ -0,0 +1,13 @@
+classdef Settings
+    %SETTINGS A structure for holding all the settings for this VIO impl.
+    
+    properties
+        nFeaturesDesired = 300;
+        patchHalfSize = 5;
+    end
+    
+    methods
+        
+    end
+end
+
