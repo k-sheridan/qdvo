6fcc73c4fef173b102d82494889dd1a0b89cdfc4
Trexter <sheridak@purdue.edu>
2018-10-04 15:35:56 -0400

    checked corner/edge cases

--- octave/SDVIO/FeatureTracking/subPixelMatchFromQuadraticInterp.m
+++ octave/SDVIO/FeatureTracking/subPixelMatchFromQuadraticInterp.m
@@ -5,23 +5,44 @@ function [pixel, cov] = subPixelMatchFromQuadraticInterp(centerPixel, neighborho
 persistent P pseudoP
 
 if isempty(P)
-    P = [1 1 1 -1 -1 1; 0 1 0 0 -1 1; 1 1 -1 1 -1 1; 1 0 0 -1 0 1; 0 0 0 0 0 1; 1 0 0 1 0 1; 1 1 -1 -1 1 1; 0 1 0 0 1 1; 1 1 1 1 1 1];
+    syms x y
+    row = [x^2, y^2, x*y, x, y, 1];
+    P = [double(subs(row, [x,y], [-1, -1]));
+         double(subs(row, [x,y], [0, -1]));
+         double(subs(row, [x,y], [1, -1]));
+         double(subs(row, [x,y], [-1, 0]));
+         double(subs(row, [x,y], [0, 0]));
+         double(subs(row, [x,y], [1, 0]));
+         double(subs(row, [x,y], [-1, 1]));
+         double(subs(row, [x,y], [0, 1]));
+         double(subs(row, [x,y], [1, 1]))]
     pseudoP = inv(P'*P)*P';
 end
 
-b = [neighborhood(1, 1:3)'; neighborhood(2, 1:3)'; neighborhood(3, 1:3)'];
+b = [neighborhood(1, 1:3)'; neighborhood(2, 1:3)'; neighborhood(3, 1:3)']
 
 coeff = pseudoP*b % computes the fit coefficients.
 
-syms x y
-fn = coeff' * [x^2;y^2;x*y;x;y;1];
+%syms x y
+%fn = coeff' * [x^2;y^2;x*y;x;y;1];
+%fcontour(fn)
 
-fsurf(fn)
+delta = [-coeff(4) / (2*coeff(1)); -coeff(5) / (2*coeff(2))]; % check for zero accelerations.
 
-pixel = centerPixel;
+cov_inv = [abs(coeff(1)), -coeff(3); -coeff(3), abs(coeff(2))];
 
-%TODO check the covariance matrix
-cov = inv(-[coeff(1), coeff(3); coeff(3), coeff(2)]);
+if (norm(coeff(1)) < 1e-8); delta(1) = 0; cov_inv(1, 1) = 1e-8; end
+if (norm(coeff(2)) < 1e-8); delta(2) = 0; cov_inv(2, 2) = 1e-8; end
+
+cov = inv(cov_inv);
+
+% check the covariance matrix
+
+
+%hold on
+%plot3(delta(1), delta(2), 0, 'o')
+
+pixel = centerPixel + delta;
 
 end
 
