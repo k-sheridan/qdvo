f48106b8f630c16b473ee89f2c714270afd34069
Trexter <sheridak@purdue.edu>
2018-11-05 13:09:39 -0500

    added a ransac fundamental matrix outlier detector

---
+++ matlab/INVIO/Helpers/FeatureTracking/computeFundamentalMatrixRANSAC.m
@@ -0,0 +1,67 @@
+function [F, chi2_avg, inlierColumns] = computeFundamentalMatrixRANSAC(bearingVec1, bearingVec2, ransacIters, ransacThreshold)
+%COMPUTEFUNDAMENTALMATRIX Use ransac to compute the fundamental matrix
+%bewteen two feature track vectors.
+% feature vector: [[x1_1;y1_1], [x1_2;y1_2], ...]
+% for this to work there must be sufficient parallax.
+% bearings must obviously be undistorted
+
+% the inlierColumns vector is a boolean vector which says which tracks are
+% correct.
+
+[m1, n1] = size(bearingVec1);
+[m2, n2] = size(bearingVec2);
+
+assert(m1 == 2 && m2 == 2)
+assert(n2 == n1)
+
+inlierMatrix = zeros(ransacIters, length(bearingVec1(1, :)));
+
+iterationInfo = {};
+
+for iterationNumber = (1:ransacIters)
+    randomColumns = randperm(n1);
+    randomColumns = randomColumns(1:8);
+    
+    % form the correspondence matrix, A, such that A*F = zeros(8, 1)
+    A = zeros(8, 9);
+    for index = (1:length(randomColumns))
+        px1 = bearingVec1(1:2, randomColumns(index));
+        px2 = bearingVec2(1:2, randomColumns(index));
+        
+        xp = px2(1);
+        yp = px2(2);
+        x = px1(1);
+        y = px1(2);
+        
+        A(index, 1:9) = [x*xp, y*xp, xp, x*yp, y*yp, yp, x, y, 1];
+    end
+    
+    % decompose A and find its null space
+    [U, S, V] = svd(A);
+    
+    fVec = V(1:9, 9)';
+    
+    F_est = [fVec(1:3); fVec(4:6); fVec(7:9)];
+    
+    % compute average squared error
+    chi2_sum = 0;
+    for index = (1:length(bearingVec1(1, :)))
+        chi2 = ([bearingVec1(1:2, index);1]' * F_est * [bearingVec2(1:2, index);1])^2;
+        chi2_sum = chi2_sum + chi2;
+        if (chi2 < ransacThreshold)
+            inlierMatrix(iterationNumber, index) = 1;
+        end
+    end
+    
+    iterationInfo{end+1} = struct('F', F_est, 'chi2_avg', chi2_sum / length(bearingVec1(1, :)));
+    
+end
+
+[val, row] = max(sum(inlierMatrix'))
+
+F = iterationInfo{row}.F;
+chi2_avg = iterationInfo{row}.chi2_avg;
+inlierColumns = inlierMatrix(row, :);
+
+end
+
---
+++ matlab/INVIO/Helpers/FeatureTracking/test/TestFundamentalOutlierDetection.m
@@ -0,0 +1,67 @@
+% this generates synthetic feature correspondences and tests the
+% fundamental matrix calculator
+
+K = [300, 0, 600; 0, 300, 600; 0, 0, 1];
+invK = inv(K);
+features1 = []; % random pixel vector
+
+for x = (-1:0.1:1)
+    for y = (-1:0.1:1)
+        px = [x;y;1];
+        features1 = [features1, px(1:2, 1)];
+    end
+end
+
+R = rotx(pi/8);
+t = [-0.1;0.1;0];
+
+features2 = [];
+
+% generate correspondences
+for index = (1:length(features1(1, :)))
+    bearing = [features1(1:2, index); 1];
+    % generate random Z
+    z =  2 + rand * 1;
+    
+    pt = z * bearing;
+    
+    ptTrans = R*pt + t;
+    
+    bearingTrans = ptTrans / ptTrans(3);
+    
+    px2 = bearingTrans;
+    
+    features2 = [features2, px2(1:2, 1)];
+end
+
+
+% compute the theoretical F
+Rinv = R';
+tinv = -R'*t;
+
+a = tinv;
+skewT = [0, -a(3), a(2); 
+        a(3), 0, -a(1); 
+        -a(2), a(1), 0];
+
+F = Rinv * skewT
+
+% manually perturb some correspondences.
+features2(1:2, 1) = features2(1:2, 1) + [0;0.5];
+
+% draw the two frames
+scatter(features1(1, :), features1(2, :))
+title('Frame 1')
+
+figure
+scatter(features2(1, :), features2(2, :))
+title('Frame 2')
+
+
+
+[F_fit, chi2avg, inliers] = computeFundamentalMatrixRANSAC(features1, features2, 20, 1e-3);
+
+F_fit
+chi2avg
+inliers(1)
+
