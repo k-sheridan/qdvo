cdadb77e91e75c1d5c709b6eb6b0a3234c412d6c
Trexter <sheridak@purdue.edu>
2019-01-23 17:52:30 -0500

    rename and add variables for the reintegrated IMU error term

---
+++ matlab/INVIO/DataStructures/InertialErrorTerm.m
@@ -0,0 +1,33 @@
+classdef InertialErrorTerm
+    %INERTIALCONSTRAINT stores the information necessary to form both a
+    %preintegrated and full inertial constraint series
+    
+    properties
+        parentKeyframeID = -1;
+        childKeyframeID = -1;
+        
+        imuMeasurementArray = {}; % cell array of IMUMeasurement
+        
+        initialized = false; % has the preintegrated measurement been computed?
+        
+        % These are the preintegrated measurements with information.
+        deltaRotation; % relative rotation between parent and child \in SO(3) (rotation matrix)
+        deltaVelocity; % change in velocity from parent to child
+        deltaPosition; % change in position from parent to child
+        
+        % biases do not change!
+        
+        % order of variables: [drot (so(3)), dvel, dpos, dbias_g, dbias_a]
+        biasJacobian; % jacobian used to make the preintegrated deltas a linear function of the biases. These must be recomputed if the bias delta is too large (TBD).
+        
+        % information matrix
+        Pinv; % 15X15 inverse covariance matrix representing the information these deltas add.
+        
+        
+    end
+    
+    methods
+        
+    end
+end
+
--- matlab/INVIO/matlab/lieAlgebra/lieBracketSe3.m
+++
@@ -1,7 +0,0 @@
-function [c] = lieBracketSe3(a, b)
-%order vx vy vz wx wy wz
-
-c(1:3, 1) = so3Hat(a(4:6, 1)) * b(1:3, 1) + so3Hat(a(1:3, 1)) * b(4:6, 1);
-c(4:6, 1) = so3Hat(a(4:6, 1)) * b(4:6, 1);
-end
-
