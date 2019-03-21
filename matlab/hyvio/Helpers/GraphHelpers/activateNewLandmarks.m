e4a3b9b8b91837de395eaccf3e7a7e33ae463ec8
Trexter <sheridak@purdue.edu>
2019-03-21 00:48:31 -0400

    nearly finished the activate landmarks function

--- matlab/hyvio/Helpers/GraphHelpers/activateNewLandmarks.m
+++ matlab/hyvio/Helpers/GraphHelpers/activateNewLandmarks.m
@@ -3,6 +3,7 @@ function [graph] = activateNewLandmarks(graph)
 %and will cover more of the first frame in the graph.
 
 s = Settings();
+rad = s.featureSeparation;
 
 % first sweep the frame list for all active keyframes (oldest to newest)
 % and project the active landmarks in those keyframe into the newest frame,
@@ -12,19 +13,17 @@ activeKeyframeIndices = [];
 [m,n] = size(graph.FrameContainer{end}.raw_image);
 spatialMask = zeros(m, n);
 
-for idx = (1:length(graph.FrameContainer))
-    if graph.FrameContainer{idx}.status == FrameStatus.ACTIVE
-        if graph.FrameContainer{idx}.isKeyframe
-            activeKeyframeIndices = [activeKeyframeIndices, idx];
-            
-            for l = graph.FrameContainer{idx}.landmarks
-                % project the feature into the newest frame.
-                
-            end
-            
-        else
-            error('Active Frame is not a keyframe.')
-        end
+[idArray, activeVisible] = computeVisibleLandmarks(graph.FrameContainer{end}, graph, true, false);
+
+for idx = (1:length(idArray))
+    l = graph.getLandmark(idArray{idx}{1}, idArray{idx}{2});
+    if l.status == LandmarkStatus.ACTIVE
+        % mark the spatial mask
+        px = activeVisible{idx};
+        spatialMask(max(px(2)-rad, 1):min(px(2)+rad, m), max(px(1)-rad, 1):min(px(1)+rad, n)) = 1;
     end
 end
 
+% now sweep again, but activate landmarks which are in new areas.
+error('ndofjasldjfhalksjdf')   
+end
