8eb0f0ea9d86eab0a522039752537c1abb68829e
Trexter <sheridak@purdue.edu>
2019-04-04 14:29:17 -0400

    quickly hacking together a rad tan distortion model

---
+++ matlab/hyvio/CameraModel/RadTanCameraModel.m
@@ -0,0 +1,217 @@
+classdef RadTanCameraModel
+    %Simple Radial Tangential distortion model.
+    
+    properties
+        radiusLookupTable; % nx2 array of [theta, radius], lookup is O(log(n)) with binary search
+        coeffs;
+        f;
+        c;
+        fov;
+        attenuation; % an array the size of the image which describes the lense attenuation. (default is ones)
+        size;
+    end
+    
+    methods
+        function obj = RadTanCameraModel(distortionCoeffs, fov, focal, principal, size, n, attenuation)
+            %EQUIDISTANTCAMERAMODEL Creates an instance of the camera
+            %model. n is optional and determines the resolution of the
+            %lookup table.
+            % This adds a 1 to the front of the distortion coeff vec.
+            if (nargin <= 6)
+                attenuation = ones(size);
+                if (nargin <= 5)
+                    n = 10000;
+                end
+            end
+            
+            obj.attenuation = attenuation;
+            obj.size = size;
+            
+            obj.f = focal;
+            obj.c = principal;
+            obj.fov = fov;
+            
+            thetaMax = pi/2; % the maximum theta value in the lookup table
+            obj.radiusLookupTable = zeros(n, 2);
+            
+            obj.radiusLookupTable(:, 1) = linspace(0, norm(size), n);
+            
+            assert(length(distortionCoeffs) == 4);
+            
+            obj.coeffs = [distortionCoeffs]; % add a 1 on the lowest order.
+            % generate the radius part of the table
+            for tableIndex = (1:n)
+                multiplier = 1 + obj.coeffs(1)*obj.radiusLookupTable(tableIndex, 1)^2 +  obj.coeffs(2)*obj.radiusLookupTable(tableIndex, 1)^4;
+                
+                obj.radiusLookupTable(tableIndex, 2) = multiplier;
+            end
+        end
+        
+        function [pixel, projectJacobian] = project(obj, pointInCameraFrame)
+            %Project a point in the camera frame into level 0 distorted pixel
+            %coordinates. done in the same fashion as kalibr.
+            % The projection jacobian maps UNIT PLANE bearing error to
+            % pixel error.
+            
+            
+            if (pointInCameraFrame(3) <= 1e-8)
+                error('point is behind camera');
+            end
+            
+            pointInCameraFrame = pointInCameraFrame / pointInCameraFrame(3); % make point homogenous.
+            
+            pxRect = [obj.f(1)*pointInCameraFrame(1) + obj.c(1); obj.f(2)*pointInCameraFrame(2) + obj.c(2)];
+            
+            r = norm(pxRect);
+            
+            error('here')
+            
+            psi = atan2(pointInCameraFrame(2), pointInCameraFrame(1));
+            
+            radius = 0;
+            for coeffIndex = (1:length(obj.coeffs))
+                radius = radius + obj.coeffs(coeffIndex) * theta^obj.order(coeffIndex);
+            end
+            
+            bearingDistorted = [radius * cos(psi); radius * sin(psi); 1];
+            
+            pixel = [bearingDistorted(1) * obj.f(1) + obj.c(1); bearingDistorted(2) * obj.f(2) + obj.c(2)];
+            
+            % if the projection jacobian is desired compute it.
+            if (nargout == 2)
+                % [dx; dy] = J * [du; dv] <= (unit plane)
+                delta = 1e-4;
+                bearing = [norm(pointInCameraFrame(1:2)) * [cos(psi); sin(psi)]; 1];
+                
+                u2 = obj.project(bearing + [delta;0;0]);
+                u1 = obj.project(bearing + [-delta;0;0]);
+                
+                v2 = obj.project(bearing + [0;delta;0]);
+                v1 = obj.project(bearing + [0;-delta;0]);
+                
+                projectJacobian = [1/(2*delta)*(u2-u1), 1/(2*delta)*(v2-v1)];
+            end
+        end
+        
+        function [bearing, unprojectJacobian] = unproject(obj, pixel)
+            % undistorts the pixel onto the image plane in normalized
+            % (metric) coordinates.
+            % the only way to break this is by asking for a out of bounds
+            % pixel like [x > 1024; 0]
+            
+            % step 1 normalize the pixel to distorted homogenous
+            % coordinates.
+            
+            
+            uvd = [(pixel(1) - obj.c(1)) / obj.f(1); (pixel(2) - obj.c(2)) / obj.f(2)];
+            
+            psi = atan2(uvd(2), uvd(1)); % used to reproduce the unit vector robustly
+            
+            % step 2 binary search the lookup table for the distorted
+            % radius
+            radiusDesired = norm(uvd); % distorted radius.
+            
+            % this assumes that radius increases with theta
+            high = length(obj.radiusLookupTable(:, 1));
+            low = 1;
+            mid = floor((high - low) / 2);
+            
+            while (1)
+                
+                eval = obj.radiusLookupTable(mid, 2);
+           
+                if (high - 1 <= low)
+                    break;
+                end
+                
+                
+                if (radiusDesired > eval)
+                    low = mid;
+                    mid = floor(low + (high - low) / 2);
+                elseif (radiusDesired < eval)
+                    high = mid;
+                    mid = floor(low + (high - low) / 2);
+                else
+                    break;
+                end
+                
+            end
+            
+            theta0 = obj.radiusLookupTable(mid, 2);
+            
+            if abs(theta0) > obj.fov/2
+                error('unproject in unstable region!');
+            end
+            
+            % FINAL STEP minimize the squared radius error
+            % r(theta0 + dtheta) ~= r0 + dr_dth(r0)*dtheta
+            % rd - r0 - dr_dth(r0)*dtheta = 0
+            % => dtheta = (rd - r0) / dr_dth(r0)
+            % iterate until convergence
+            
+            theta = theta0;
+            for iter = (1:20) % should converge way before 20 iters depending on table resolution
+                der = obj.distortionDerivativeFn(theta);
+                
+                if (norm(der) <= 1e-8)
+                    break;
+                end
+                
+                theta = theta + (radiusDesired - obj.distortionFn(theta)) / der;
+            end
+            
+            bearing = [tan(theta) * [cos(psi); sin(psi)]; 1];
+            
+            % if the unproject jacobian is desired use the new bearing to
+            % compute the projection jacobian and invert it.
+            if (nargout == 2)
+                [px, pJ] = obj.project(bearing);
+                
+                unprojectJacobian = inv(pJ);
+            end
+            
+        end
+        
+        function [radius] = distortionFn(obj, theta)
+            % evaluates the equidistant distortion function for this model
+            
+            radius = 0;
+            for coeffIndex = (1:length(obj.coeffs))
+                 radius = radius + obj.coeffs(coeffIndex) * theta^obj.order(coeffIndex);
+            end
+        end
+        
+        function [dradius_dtheta] = distortionDerivativeFn(obj, theta)
+            % evaluates the derivative of the equidistant distortion function for this model
+            
+            dradius_dtheta = 0;
+            for coeffIndex = (1:length(obj.coeffs))
+                 dradius_dtheta = dradius_dtheta + obj.order(coeffIndex) * obj.coeffs(coeffIndex) * theta^(obj.order(coeffIndex) - 1);
+            end
+        end
+        
+        function [onImage] = isPixelOnImage(obj, px)
+            persistent buffer;
+            persistent attenuationMinimum; 
+            attenuationMinimum = 0;
+            buffer = 2;
+            if px(1) <= buffer || px(1) >= (obj.size(1) - buffer)
+                onImage = false;
+                return
+            end
+            
+            if px(2) <= buffer || px(2) >= (obj.size(2) - buffer)
+                onImage = false;
+                return
+            end
+            
+            if obj.attenuation(round(px(2)), round(px(1))) <= attenuationMinimum
+                onImage = false;
+                return
+            end
+            
+            onImage = true;
+        end
+    end
+end
+
---
+++ matlab/hyvio/CameraModel/TestCameraModelRadTan.m
@@ -0,0 +1,46 @@
+vignette = imread('vignette.png', 'PNG');
+
+cm = RadTanCameraModel([-0.28340811, 0.07395907, 0.00019359, 1.76187114e-05],...
+    pi, [458.654, 457.296], [367.215, 248.375], [752, 480], 10000, vignette)
+
+points = [];
+for x = (-5:0.2:5)
+    for y = (-5:0.2:5)
+        pixel = cm.project([x;y;1]);
+        if (pixel(1) >= 0 && pixel(1) <= 1024 && pixel(2) >= 0 && pixel(2) <= 1024)
+            points = [points, pixel];
+        end
+    end
+end
+
+%imshow(vignette)
+hold on
+scatter(points(1, :), points(2, :), 'x')
+xlim([-150, 1200])
+ylim([-150, 1200])
+daspect([1, 1 ,1])
+title('RadTan Distortion Model: 10mX10m Grid 1 meter from camera')
+xlabel('pixel')
+ylabel('pixel')
+grid on
+
+
+% test project jacobians
+pos = [0.1;-0.2;2];
+J = zeros(2, 3);
+dx = zeros(3, 1);
+delta = 1e-4;
+
+for idx = (1:3)
+    dx(idx) = dx(idx) + delta;
+    pxHigh = cm.project(pos+dx);
+    dx(idx) = dx(idx) - 2*delta;
+    pxLow = cm.project(pos+dx);
+    dx(idx) = dx(idx) + delta;
+    J(1:2, idx) = (pxHigh-pxLow)/(2*delta);
+end
+
+[px,jac] = cm.project(pos);
+
+jac * [1/pos(3), 0, -pos(1)/pos(3)^2; 0, 1/pos(3), -pos(2)/pos(3)^2]
+J
