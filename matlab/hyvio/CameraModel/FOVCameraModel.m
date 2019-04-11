04c8e202ac152f50f4ee479819802d35e0d7b9cb
Trexter <sheridak@purdue.edu>
2019-04-11 12:44:05 -0400

    adding FOV distortion model

--- matlab/hyvio/CameraModel/FOVCameraModel.m
+++ matlab/hyvio/CameraModel/FOVCameraModel.m
@@ -1,8 +1,12 @@
-classdef FOVCameraModel
-    %Simple Radial Tangential distortion model.
+classdef EquidistantCameraModel
+    %EQUIDISTANTCAMERAMODEL implementation of the equidistant camera model
+    %used by Kalibr. Both the projection and unprojection jacobians are
+    %computed numerically with a first order finite difference.
     
     properties
-        w;
+        radiusLookupTable; % nx2 array of [theta, radius], lookup is O(log(n)) with binary search
+        order;
+        coeffs;
         f;
         c;
         fov;
@@ -11,11 +15,10 @@ classdef FOVCameraModel
     end
     
     methods
-        function obj = RadTanCameraModel(w, fov, focal, principal, size, n, attenuation)
+        function obj = EquidistantCameraModel(distortionCoeffs, fov, focal, principal, size, n, attenuation)
             %EQUIDISTANTCAMERAMODEL Creates an instance of the camera
             %model. n is optional and determines the resolution of the
             %lookup table.
-            % order follows opencv order: [k1, k2, p1, p2]
             % This adds a 1 to the front of the distortion coeff vec.
             if (nargin <= 6)
                 attenuation = ones(size);
@@ -31,12 +34,22 @@ classdef FOVCameraModel
             obj.c = principal;
             obj.fov = fov;
             
-            if obj.fov >= pi*0.98
-                error('fov too large for rad tan');
+            thetaMax = pi/2; % the maximum theta value in the lookup table
+            obj.radiusLookupTable = zeros(n, 2);
+            
+            obj.radiusLookupTable(:, 1) = linspace(0, thetaMax, n);
+            
+            obj.coeffs = [1, distortionCoeffs]; % add a 1 on the lowest order.
+            obj.order = (1:2:1 + (length(obj.coeffs)-1) * 2) % generate the orders of each term
+            % generate the radius part of the table
+            for tableIndex = (1:n)
+                radius = 0;
+                for coeffIndex = (1:length(obj.coeffs))
+                    radius = radius + obj.coeffs(coeffIndex) * obj.radiusLookupTable(tableIndex, 1)^obj.order(coeffIndex);
+                end
+                
+                obj.radiusLookupTable(tableIndex, 2) = radius;
             end
-            
-            
-            obj.w = w;
         end
         
         function [pixel, projectJacobian] = project(obj, pointInCameraFrame)
@@ -52,13 +65,22 @@ classdef FOVCameraModel
             
             pointInCameraFrame = pointInCameraFrame / pointInCameraFrame(3); % make point homogenous.
             
-            r_u = norm(pointInCameraFrame(1:2, 1));
+            theta = atan2(sqrt(pointInCameraFrame(1)^2 + pointInCameraFrame(2)^2), abs(pointInCameraFrame(3)));
+            
+            if abs(theta) > obj.fov/2
+                error('point out of fov');
+            end
             
             psi = atan2(pointInCameraFrame(2), pointInCameraFrame(1));
             
-            uvd = [cos(psi);sin(psi)] * 1/obj.w * atan(2*r_u*tan(obj.w/2));
+            radius = 0;
+            for coeffIndex = (1:length(obj.coeffs))
+                radius = radius + obj.coeffs(coeffIndex) * theta^obj.order(coeffIndex);
+            end
             
-            pixel = [obj.f(1)*uvd(1) + obj.c(1); obj.f(2)*uvd(2) + obj.c(2)];
+            bearingDistorted = [radius * cos(psi); radius * sin(psi); 1];
+            
+            pixel = [bearingDistorted(1) * obj.f(1) + obj.c(1); bearingDistorted(2) * obj.f(2) + obj.c(2)];
             
             if ~obj.isPixelOnImage(pixel)
                 error('pixel not on image');
@@ -68,7 +90,7 @@ classdef FOVCameraModel
             if (nargout == 2)
                 % [dx; dy] = J * [du; dv] <= (unit plane)
                 delta = 1e-4;
-                bearing = [pointInCameraFrame(1:2); 1];
+                bearing = [norm(pointInCameraFrame(1:2)) * [cos(psi); sin(psi)]; 1];
                 
                 u2 = obj.project(bearing + [delta;0;0]);
                 u1 = obj.project(bearing + [-delta;0;0]);
@@ -83,15 +105,19 @@ classdef FOVCameraModel
         function [bearing, unprojectJacobian] = unproject(obj, pixel)
             % undistorts the pixel onto the image plane in normalized
             % (metric) coordinates.
-            % 
+            % the only way to break this is by asking for a out of bounds
+            % pixel like [x > 1024; 0]
+            
+            % step 1 normalize the pixel to distorted homogenous
+            % coordinates.
             
             
             uvd = [(pixel(1) - obj.c(1)) / obj.f(1); (pixel(2) - obj.c(2)) / obj.f(2)];
             
             psi = atan2(uvd(2), uvd(1)); % used to reproduce the unit vector robustly
             
-            
-            % find the corrected radius
+            % step 2 binary search the lookup table for the distorted
+            % radius
             radiusDesired = norm(uvd); % distorted radius.
             
             % this assumes that radius increases with theta
@@ -120,20 +146,30 @@ classdef FOVCameraModel
                 
             end
             
-            newRadius = obj.radiusLookupTable(mid, 1);
+            theta0 = obj.radiusLookupTable(mid, 2);
             
-            bearing0 = newRadius * [cos(psi); sin(psi)];
-            
-            % finally, iteratively refine the bearing.
-            bearing = bearing0;
-            for iter = (1:10)
-                % standard nonlinear least squares
-                [pxCurr, projJac] = obj.project([bearing; 1]);
-                r = pxCurr - pixel;
-                delta = (projJac'*projJac)\(-projJac' * r);
-                bearing = bearing + delta;
+            if abs(theta0) > obj.fov/2
+                error('unproject in unstable region!');
             end
             
+            % FINAL STEP minimize the squared radius error
+            % r(theta0 + dtheta) ~= r0 + dr_dth(r0)*dtheta
+            % rd - r0 - dr_dth(r0)*dtheta = 0
+            % => dtheta = (rd - r0) / dr_dth(r0)
+            % iterate until convergence
+            
+            theta = theta0;
+            for iter = (1:20) % should converge way before 20 iters depending on table resolution
+                der = obj.distortionDerivativeFn(theta);
+                
+                if (norm(der) <= 1e-8)
+                    break;
+                end
+                
+                theta = theta + (radiusDesired - obj.distortionFn(theta)) / der;
+            end
+            
+            bearing = [tan(theta) * [cos(psi); sin(psi)]; 1];
             
             % if the unproject jacobian is desired use the new bearing to
             % compute the projection jacobian and invert it.
@@ -145,10 +181,27 @@ classdef FOVCameraModel
             
         end
         
+        function [radius] = distortionFn(obj, theta)
+            % evaluates the equidistant distortion function for this model
+            
+            radius = 0;
+            for coeffIndex = (1:length(obj.coeffs))
+                 radius = radius + obj.coeffs(coeffIndex) * theta^obj.order(coeffIndex);
+            end
+        end
+        
+        function [dradius_dtheta] = distortionDerivativeFn(obj, theta)
+            % evaluates the derivative of the equidistant distortion function for this model
+            
+            dradius_dtheta = 0;
+            for coeffIndex = (1:length(obj.coeffs))
+                 dradius_dtheta = dradius_dtheta + obj.order(coeffIndex) * obj.coeffs(coeffIndex) * theta^(obj.order(coeffIndex) - 1);
+            end
+        end
         
         function [onImage] = isPixelOnImage(obj, px)
-            %persistent buffer;
-            %persistent attenuationMinimum; 
+            persistent buffer;
+            persistent attenuationMinimum; 
             attenuationMinimum = 0;
             buffer = 2;
             if px(1) <= buffer || px(1) >= (obj.size(1) - buffer)
@@ -161,10 +214,10 @@ classdef FOVCameraModel
                 return
             end
             
-%             if obj.attenuation(round(px(2)), round(px(1))) <= attenuationMinimum
-%                 onImage = false;
-%                 return
-%             end
+            if obj.attenuation(round(px(2)), round(px(1))) <= attenuationMinimum
+                onImage = false;
+                return
+            end
             
             onImage = true;
         end
