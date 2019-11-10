1c5f5d272209b3b67e73397acdc7cef77120c84f
Trexter <sheridak@purdue.edu>
2019-11-09 19:50:24 -0800

    add slot array based partitioned matrix to the PSD Linear Systen

--- src/Optimizer/PSDLinearSystem.h
+++ src/Optimizer/PSDLinearSystem.h
@@ -3,11 +3,31 @@
 #include "Containers.h"
 #include "MetaHelpers.h"
 #include "GaussianPrior"
+#include "SlotMap.h"
+#include "SlotArray.h"
+
+namespace ArgMin {
 
 template <typename... T>
 class PSDLinearSystem;
 
 template <typename ScalarType, typename... ErrorTerms, typename... Variables>
 class PSDLinearSystem<Scalar<ScalarType>, ErrorTermGroup<ErrorTerms...>, VariableGroup<Variables...>> {
+public:
+
+template <typename VariableType>
+using BVector = SlotArray<VariableKey<VariableType>, Eigen::Matrix<ScalarType, Eigen::Dynamic, VariableType::dimension>;
+template <typename VariableType>
+using DBlock = Eigen::Matrix<ScalarType, VariableType::dimension, VariableType::dimension>;
+template <typename VariableType>
+using DVector = SlotArray<VariableKey<VariableType>, DBlock<VariableType>>;
+
+
+Eigen::Matrix<ScalarType, Eigen::Dynamic, Eigen::Dynamic> A; // Dense matrix in the upper right corner
+std::tuple<BVector<Variables>>...> B; // Top right block matrix. Equal to bottom left transposed.
+std::tuple<DVector<Variables>>...> D; // Block diagonal matrix in the bottom right. Each block is invertible.
+
 
 };
+
+} // namespace ArgMin
