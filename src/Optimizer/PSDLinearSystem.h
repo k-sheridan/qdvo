42722f1c9bcf2ab3bc6a801a3e2e554fcd14a2d4
Trexter <sheridak@purdue.edu>
2019-11-10 17:16:57 -0800

    added solve and initialize functions to the solver.

--- src/Optimizer/PSDLinearSystem.h
+++ src/Optimizer/PSDLinearSystem.h
@@ -25,8 +25,8 @@ class PSDLinearSystem;
  * It is assumed that variables are unique across both variable sets.
  * For example: CorrelatedSet = {T1, T2, T3}, Uncorrelated Set = {T4, T5, T6}
  */
-template <typename ScalarType, typename... ErrorTerms, typename... CorrelatedVariables, typename... UncorrelatedVariables>
-class PSDLinearSystem<Scalar<ScalarType>, ErrorTermGroup<ErrorTerms...>, VariableGroup<CorrelatedVariables...>, VariableGroup<UncorrelatedVariables...>>
+template <typename ScalarType, typename... ErrorTerms, typename... Variables, typename... UncorrelatedVariables>
+class PSDLinearSystem<Scalar<ScalarType>, ErrorTermGroup<ErrorTerms...>, VariableGroup<Variables...>, VariableGroup<UncorrelatedVariables...>>
 {
 public:
     template <typename VariableType>
@@ -40,8 +40,30 @@ public:
     std::tuple<BVector<UncorrelatedVariables>...> B;             // Top right block matrix. Equal to bottom left transposed.
     std::tuple<DVector<UncorrelatedVariables>...> D;             // Block diagonal matrix in the bottom right. Each block is invertible.
 
+    Eigen::Matrix<ScalarType, Eigen::Dynamic, 1> dx; // Pre allocated solution vector.
+    BlockVector<Scalar<ScalarType>, Dimension<1>, VariableGroup<Variables...>> dxBlockVector; // Block vector form of the dx vector. Used to solve ordering issues.
+    
+    Eigen::Matrix<ScalarType, Eigen::Dynamic, 1> b; // Preallocated rhs vector.
+
     PSDLinearSystem()
     {
+    }
+
+    /**
+     * This function is ran once before a solve. This could be used to preallocate memory or precompute values.
+     */
+    void initialize(VariableContainer<Variables...>& variables, ErrorTermContainer<ErrorTerms...>& errorTerms) {
+
+    }
+
+    /**
+     * This function will linearize the error terms if necessary, build the problem, and solve for the perturbation
+     * while exploiting the knowledge of the uncorrelated variable set.
+     * 
+     * After the solve, it can be assumed that the error terms are in the linearized state used for the final iteration.
+     */
+    void solve(VariableContainer<Variables...>& variables, ErrorTermContainer<ErrorTerms...>& errorTerms) {
+
     }
 };
 
